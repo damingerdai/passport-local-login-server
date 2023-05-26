bcce466ee1e118e654a34b669f0b4b8387c05da1
Arthur Ming <guobin.ming@pwc.com>
2023-05-26 14:12:20 +0800

    chore: init commit

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import express from 'express';
+import * as http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+const instance = server.listen('8080', () => {
+  console.log('oauth2 mock login server is now running on http://localhost:8080');
+});
+
+instance.setTimeout(3 * 60 * 60 * 1000);
